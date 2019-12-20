6f6ca7e64df6baf31b1c4133f5f45fcae2fe31c3
Jake Van Alstyne 🎩 <jakeva@gmail.com>
2019-12-20 15:28:54 -0700

    Make it a motion detection

--- AVCamFilterMacOS/CameraViewController.swift
+++ AVCamFilterMacOS/CameraViewController.swift
@@ -1,9 +1,9 @@
 /*
-See LICENSE folder for this sample’s licensing information.
+ See LICENSE folder for this sample’s licensing information.
 
-Abstract:
-The view controller for the AVCamFilter camera interface.
-*/
+ Abstract:
+ The view controller for the AVCamFilter camera interface.
+ */
 
 import Cocoa
 import AVFoundation
@@ -13,11 +13,13 @@ class CameraViewController: NSViewController, AVCaptureVideoDataOutputSampleBuff
   private let captureSession = AVCaptureSession()
   private var videoInput: AVCaptureDeviceInput!
   private let videoDataOutput = AVCaptureVideoDataOutput()
-  private var videoFilter = RosyMetalRenderer()
+  private var videoFilter = MotionDetectMetalRenderer()
   private let sessionQueue = DispatchQueue(label: "SessionQueue", attributes: [], autoreleaseFrequency: .workItem)
   private let dataOutputQueue = DispatchQueue(label: "VideoDataQueue", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
   private var renderingEnabled = true
   private var isSessionRunning = false
+  private var lastSampleBuffer: CMSampleBuffer!
+  private var currentSampleBuffer: CMSampleBuffer!
 
   private enum SessionSetupResult {
     case success
@@ -26,142 +28,149 @@ class CameraViewController: NSViewController, AVCaptureVideoDataOutputSampleBuff
   }
 
   private var setupResult: SessionSetupResult = .success
-    // MARK: - View Controller Life Cycle
+  // MARK: - View Controller Life Cycle
 
-    override func viewDidLoad() {
-      switch AVCaptureDevice.authorizationStatus(for: .video) {
-        case .authorized:
-          // The user has previously granted access to the camera
-          break
+  override func viewDidLoad() {
+    switch AVCaptureDevice.authorizationStatus(for: .video) {
+    case .authorized:
+      // The user has previously granted access to the camera
+      break
 
-        case .notDetermined:
-          /*
-           The user has not yet been presented with the option to grant video access
-           Suspend the SessionQueue to delay session setup until the access request has completed
-           */
-          sessionQueue.suspend()
-          AVCaptureDevice.requestAccess(for: .video, completionHandler: { granted in
-            if !granted {
-              self.setupResult = .notAuthorized
-            }
-            self.sessionQueue.resume()
-          })
+    case .notDetermined:
+      /*
+       The user has not yet been presented with the option to grant video access
+       Suspend the SessionQueue to delay session setup until the access request has completed
+       */
+      sessionQueue.suspend()
+      AVCaptureDevice.requestAccess(for: .video, completionHandler: { granted in
+        if !granted {
+          self.setupResult = .notAuthorized
+        }
+        self.sessionQueue.resume()
+      })
 
-        default:
-          // The user has previously denied access
-          setupResult = .notAuthorized
-      }
-
-      sessionQueue.async {
-        self.configureSession()
-      }
+    default:
+      // The user has previously denied access
+      setupResult = .notAuthorized
     }
 
-    override func viewWillAppear() {
-      super.viewWillAppear()
+    sessionQueue.async {
+      self.configureSession()
+    }
+  }
 
-      sessionQueue.async {
-        switch self.setupResult {
-          case .success:
-            self.dataOutputQueue.async {
-              self.renderingEnabled = true
-            }
+  override func viewWillAppear() {
+    super.viewWillAppear()
 
-            self.captureSession.startRunning()
-            self.isSessionRunning = self.captureSession.isRunning
-          case .notAuthorized:
-            NSLog("Not authorized")
-          case .configurationFailed:
-            NSLog("Configuration failed")
+    sessionQueue.async {
+      switch self.setupResult {
+      case .success:
+        self.dataOutputQueue.async {
+          self.renderingEnabled = true
         }
+
+        self.captureSession.startRunning()
+        self.isSessionRunning = self.captureSession.isRunning
+      case .notAuthorized:
+        NSLog("Not authorized")
+      case .configurationFailed:
+        NSLog("Configuration failed")
       }
     }
+  }
 
-    override func viewWillDisappear() {
-        dataOutputQueue.async {
-            self.renderingEnabled = false
-        }
-        sessionQueue.async {
-            if self.setupResult == .success {
-                self.captureSession.stopRunning()
-                self.isSessionRunning = self.captureSession.isRunning
-            }
-        }
+  override func viewWillDisappear() {
+    dataOutputQueue.async {
+      self.renderingEnabled = false
+    }
+    sessionQueue.async {
+      if self.setupResult == .success {
+        self.captureSession.stopRunning()
+        self.isSessionRunning = self.captureSession.isRunning
+      }
+    }
+  }
+
+  // MARK: - Session Management
+
+  // Call this on the SessionQueue
+  private func configureSession() {
+    if setupResult != .success {
+      return
     }
 
-    // MARK: - Session Management
+    let defaultVideoDevice = AVCaptureDevice.default(for: AVMediaType.video)
 
-    // Call this on the SessionQueue
-    private func configureSession() {
-      if setupResult != .success {
-        return
-      }
+    guard let videoDevice = defaultVideoDevice else {
+      print("Could not find any video device")
+      setupResult = .configurationFailed
+      return
+    }
 
-      let defaultVideoDevice = AVCaptureDevice.default(for: AVMediaType.video)
+    do {
+      videoInput = try AVCaptureDeviceInput(device: videoDevice)
+    } catch {
+      print("Could not create video device input: \(error)")
+      setupResult = .configurationFailed
+      return
+    }
 
-      guard let videoDevice = defaultVideoDevice else {
-        print("Could not find any video device")
-        setupResult = .configurationFailed
-        return
-      }
+    let formatDescription = videoDevice.activeFormat.formatDescription
+    let dimensions = CMVideoFormatDescriptionGetDimensions(formatDescription)
+    let resolution = CGSize(width: CGFloat(dimensions.width), height: CGFloat(dimensions.height))
+    DispatchQueue.main.async {
+      self.view.window?.contentAspectRatio = resolution
+    }
 
-      do {
-        videoInput = try AVCaptureDeviceInput(device: videoDevice)
-      } catch {
-        print("Could not create video device input: \(error)")
-        setupResult = .configurationFailed
-        return
-      }
+    captureSession.beginConfiguration()
 
-      let formatDescription = videoDevice.activeFormat.formatDescription
-      let dimensions = CMVideoFormatDescriptionGetDimensions(formatDescription)
-      let resolution = CGSize(width: CGFloat(dimensions.width), height: CGFloat(dimensions.height))
-      DispatchQueue.main.async {
-        self.view.window?.contentAspectRatio = resolution
-      }
+    //    captureSession.sessionPreset = AVCaptureSession.Preset.low
 
-      captureSession.beginConfiguration()
-
-      //    captureSession.sessionPreset = AVCaptureSession.Preset.low
-
-      guard captureSession.canAddInput(videoInput) else {
-        print("Could not add video device input to the session")
-        setupResult = .configurationFailed
-        captureSession.commitConfiguration()
-        return
-      }
-      captureSession.addInput(videoInput)
-
-      if captureSession.canAddOutput(videoDataOutput) {
-        captureSession.addOutput(videoDataOutput)
-        videoDataOutput.videoSettings = [
-          kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA),
-          kCVPixelBufferMetalCompatibilityKey as String: true
-        ]
-        videoDataOutput.setSampleBufferDelegate(self, queue: dataOutputQueue)
-      } else {
-        print("Could not add video data output to the session")
-        setupResult = .configurationFailed
-        captureSession.commitConfiguration()
-        return
-      }
-
+    guard captureSession.canAddInput(videoInput) else {
+      print("Could not add video device input to the session")
+      setupResult = .configurationFailed
       captureSession.commitConfiguration()
+      return
+    }
+    captureSession.addInput(videoInput)
+
+    if captureSession.canAddOutput(videoDataOutput) {
+      captureSession.addOutput(videoDataOutput)
+      videoDataOutput.videoSettings = [
+        kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA),
+        kCVPixelBufferMetalCompatibilityKey as String: true
+      ]
+      videoDataOutput.setSampleBufferDelegate(self, queue: dataOutputQueue)
+    } else {
+      print("Could not add video data output to the session")
+      setupResult = .configurationFailed
+      captureSession.commitConfiguration()
+      return
     }
 
-    // MARK: - Video Data Output Delegate
+    captureSession.commitConfiguration()
+  }
 
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        processVideo(sampleBuffer: sampleBuffer)
+  // MARK: - Video Data Output Delegate
+
+  func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+    if self.currentSampleBuffer == nil {
+      self.lastSampleBuffer = sampleBuffer
+    } else {
+      self.lastSampleBuffer = self.currentSampleBuffer
     }
+    self.currentSampleBuffer = sampleBuffer
+    processVideo(lastSampleBuffer: self.lastSampleBuffer, currentSampleBuffer: self.currentSampleBuffer)
+  }
 
-  func processVideo(sampleBuffer: CMSampleBuffer) {
+  func processVideo(lastSampleBuffer: CMSampleBuffer, currentSampleBuffer: CMSampleBuffer) {
     if !renderingEnabled {
       return
     }
 
-    guard let videoPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer),
-      let formatDescription = CMSampleBufferGetFormatDescription(sampleBuffer) else {
+    guard let lastVideoPixelBuffer = CMSampleBufferGetImageBuffer(lastSampleBuffer),
+      let currentVideoPixelBuffer = CMSampleBufferGetImageBuffer(currentSampleBuffer),
+      let formatDescription = CMSampleBufferGetFormatDescription(lastSampleBuffer) else {
         return
     }
 
@@ -174,7 +183,7 @@ class CameraViewController: NSViewController, AVCaptureVideoDataOutputSampleBuff
     }
 
     // Send the pixel buffer through the filter
-    guard let filteredBuffer = self.videoFilter.render(pixelBuffer: videoPixelBuffer) else {
+    guard let filteredBuffer = self.videoFilter.render(lastPixelBuffer: lastVideoPixelBuffer, currentPixelBuffer: currentVideoPixelBuffer) else {
       print("Unable to filter video buffer")
       return
     }
---
+++ AVCamFilterMacOS/MotionDetect.metal
@@ -0,0 +1,36 @@
+//
+//  File.metal
+//  OnCue-OSX
+//
+//  Created by Jake Van Alstyne on 12/20/19.
+//  Copyright © 2019 EggDevil. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+// Compute kernel
+kernel void motionDetect(texture2d<half, access::read>  lastTexture  [[ texture(0) ]],
+                         texture2d<half, access::read>  currentTexture  [[ texture(1) ]],
+                         texture2d<half, access::write> outputTexture [[ texture(2) ]],
+                         uint2 gid [[thread_position_in_grid]])
+{
+  // Don't read or write outside of the texture.
+  if ((gid.x >= currentTexture.get_width()) || (gid.y >= currentTexture.get_height())) {
+    return;
+  }
+
+  half4 lastInputColor = lastTexture.read(gid);
+  half4 currentInputColor = currentTexture.read(gid);
+
+  half4 difference = half4(currentInputColor.r - lastInputColor.r,
+                           currentInputColor.g - lastInputColor.g,
+                           currentInputColor.b - lastInputColor.b,
+                           1.0);
+  float deltaDotP = dot(difference, difference);
+  float red = deltaDotP;
+
+  half4 outputColor =  deltaDotP > 1.0 ? half4(red, 0.0, 0.0, 1.0) : lastInputColor;
+
+  outputTexture.write(outputColor, gid);
+}
